a811c8b6667df7d96ebfc50f66ae5c2581bbfdc5
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-07-30 10:03:00 -0500

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { UserLoginInterface } from '../interfaces/user-login.interface';
 import { environment } from '../environments/environment';
 import { UserRegisterInterface } from '../interfaces/user-register.interface';
 import { UserInterface } from '../interfaces/user.interface';
-import { DOCUMENT } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +12,14 @@ import { DOCUMENT } from '@angular/common';
 export class AuthService {
 
   private user?: UserInterface;
-  private localStorage = this.document.defaultView?.localStorage;
 
-  constructor(private http: HttpClient, @Inject(DOCUMENT) private document: Document) { }
+  constructor(private http: HttpClient) { }
 
-  public login(user: UserLoginInterface): Observable<UserLoginInterface>{
+  public login(user: UserLoginInterface): Observable<any>{
     return this.http.post<UserLoginInterface>(environment.urlSpring+'auth/login', user)
+    .pipe(
+      tap(user => this.user = user),
+    )
   }
 
   public register(user: UserRegisterInterface): Observable<UserRegisterInterface>{
