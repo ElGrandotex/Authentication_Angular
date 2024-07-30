006fccd2081552fd08c397436fd9c4cec54f3dac
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-07-30 10:03:19 -0500

    Login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { UserLoginInterface } from '../../interfaces/user-login.interface';
+import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +12,17 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginComponent {
 
+  private localStorage = this.document.defaultView?.localStorage;
   public loginForm: FormGroup = this.fb.group({
     username: ['', [Validators.required]],
     password: ['', [Validators.required]],
   })
 
   constructor(
+    @Inject(DOCUMENT) private document: Document,
     private fb: FormBuilder,
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router,
   ){}
 
   public isValidField(field:string){
@@ -25,5 +31,22 @@ export class LoginComponent {
 
   onSubmit(){
     this.loginForm.markAllAsTouched();
+    const user: UserLoginInterface = {
+      username : this.loginForm.controls['username'].value,
+      password : this.loginForm.controls['password'].value
+    }
+    if (this.loginForm.valid) {
+      this.auth.login(user).subscribe(
+        (response) => {
+          const token = response.token;
+          console.log(token);
+          this.localStorage?.setItem('token', token)
+          this.router.navigate(['/home'])
+        },
+        (error)=> {
+          console.error(error);
+        }
+      )
+    }
   }
 }
